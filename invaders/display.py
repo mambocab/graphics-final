0374bc9ebe808262bff7306af666e116be22467c
Jim Witschey <jim.witschey@gmail.com>
2014-12-11 12:56:28 -0500

    almost there with rendering an invader...

--- invaders/display.py
+++ invaders/display.py
@@ -3,6 +3,7 @@ from functools import partial
 from models import ALIEN_1
 from linearalgebra import normal_vector
 from contextlib import contextmanager
+from collections import deque
 
 @contextmanager
 def enabled(gl_setting):
@@ -28,46 +29,51 @@ rtri = 0.0
 # Rotation angle for the quadrilateral.
 rquad = 0.0
 
+consume = lambda iterator: deque(iterator, maxlen=0)
+
 def draw_scene(gl, glut):
     global rtri, rquad
 
     pre_draw(gl, glut)
 
     # reset view to initialized view
-    gl.glTranslatef(-1.5, 0.0, -6.0)
+    # gl.glTranslatef(-1.5, 0.0, -6.0)
 
     # rotate pyramid-building space by rtri on y axis
     gl.glRotatef(rtri, 0.0, 1.0, 0.0)
 
     with gl_environment(gl, gl.GL_TRIANGLES):
-        gl.glColor3f(1.0, 0.0, 0.0)  # Red
-        gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Front)
-        gl.glColor3f(0.0, 1.0, 0.0)  # Green
-        gl.glVertex3f(-1.0, -1.0, 1.0)  # Left Of Triangle (Front)
-        gl.glColor3f(0.0, 0.0, 1.0)  # Blue
-        gl.glVertex3f(1.0, -1.0, 1.0)
+        gl.glColor3f(1.0, 0.0, 0.0)
+        for i, tri in enumerate(ALIEN_1):
+            consume(map(gl.glVertex3fv, tri))
 
-        gl.glColor3f(1.0, 0.0, 0.0)  # Red
-        gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Right)
-        gl.glColor3f(0.0, 0.0, 1.0)  # Blue
-        gl.glVertex3f(1.0, -1.0, 1.0)  # Left Of Triangle (Right)
-        gl.glColor3f(0.0, 1.0, 0.0)  # Green
-        gl.glVertex3f(1.0, -1.0, -1.0)  # Right
+        # gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Front)
+        # gl.glColor3f(0.0, 1.0, 0.0)  # Green
+        # gl.glVertex3f(-1.0, -1.0, 1.0)  # Left Of Triangle (Front)
+        # gl.glColor3f(0.0, 0.0, 1.0)  # Blue
+        # gl.glVertex3f(1.0, -1.0, 1.0)
 
-        gl.glColor3f(1.0, 0.0, 0.0)  # Red
-        gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Back)
-        gl.glColor3f(0.0, 1.0, 0.0)  # Green
-        gl.glVertex3f(1.0, -1.0, -1.0)  # Left Of Triangle (Back)
-        gl.glColor3f(0.0, 0.0, 1.0)  # Blue
-        gl.glVertex3f(-1.0, -1.0, -1.0)  # Right Of
+        # gl.glColor3f(1.0, 0.0, 0.0)  # Red
+        # gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Right)
+        # gl.glColor3f(0.0, 0.0, 1.0)  # Blue
+        # gl.glVertex3f(1.0, -1.0, 1.0)  # Left Of Triangle (Right)
+        # gl.glColor3f(0.0, 1.0, 0.0)  # Green
+        # gl.glVertex3f(1.0, -1.0, -1.0)  # Right
+
+        # gl.glColor3f(1.0, 0.0, 0.0)  # Red
+        # gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Back)
+        # gl.glColor3f(0.0, 1.0, 0.0)  # Green
+        # gl.glVertex3f(1.0, -1.0, -1.0)  # Left Of Triangle (Back)
+        # gl.glColor3f(0.0, 0.0, 1.0)  # Blue
+        # gl.glVertex3f(-1.0, -1.0, -1.0)  # Right Of
 
 
-        gl.glColor3f(1.0, 0.0, 0.0)  # Red
-        gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Left)
-        gl.glColor3f(0.0, 0.0, 1.0)  # Blue
-        gl.glVertex3f(-1.0, -1.0, -1.0)  # Left Of Triangle (Left)
-        gl.glColor3f(0.0, 1.0, 0.0)  # Green
-        gl.glVertex3f(-1.0, -1.0, 1.0)  # Right Of Triangle (Left)
+        # gl.glColor3f(1.0, 0.0, 0.0)  # Red
+        # gl.glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Left)
+        # gl.glColor3f(0.0, 0.0, 1.0)  # Blue
+        # gl.glVertex3f(-1.0, -1.0, -1.0)  # Left Of Triangle (Left)
+        # gl.glColor3f(0.0, 1.0, 0.0)  # Green
+        # gl.glVertex3f(-1.0, -1.0, 1.0)  # Right Of Triangle (Left)
 
     # What values to use?  Well, if you have a FAST machine
     # and a FAST 3D Card, then large values make an unpleasant display
