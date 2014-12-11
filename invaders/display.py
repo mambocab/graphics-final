748346ac1d03e96cbcb0346dc232e883c89124cc
Jim Witschey <jim.witschey@gmail.com>
2014-12-11 11:21:58 -0500

    move display function into display module

--- invaders/display.py
+++ invaders/display.py
@@ -4,6 +4,9 @@ from models import ALIEN_1
 from linearalgebra import normal_vector
 from contextlib import contextmanager
 
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+
 @contextmanager
 def enabled(gl_setting):
     gl.glEnable(gl_setting)
@@ -16,12 +19,6 @@ def gl_environment(gl, gl_env):
     yield
     gl.glEnd()
 
-@contextmanager
-def gl_push_and_pop(gl):
-    gl.glPushMatrix()
-    yield
-    gl.glPopMatrix()
-
 def set_up(gl, glut):
     # clears screen
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
@@ -30,42 +27,66 @@ def set_up(gl, glut):
     # marks buffer for redrawing
     glut.glutPostRedisplay()
 
-def set_vertices(gl):
+# Rotation angle for the triangle.
+rtri = 0.0
 
-    # set lighting values
+# Rotation angle for the quadrilateral.
+rquad = 0.0
 
-    with gl_environment(gl, gl.GL_TRIANGLES):
+def draw_scene():
+    global rtri, rquad
 
-        lightings = ((gl.GL_AMBIENT, ALIEN_1.material.ambient),
-                     (gl.GL_DIFFUSE, ALIEN_1.material.diffuse),
-                     (gl.GL_SPECULAR, ALIEN_1.material.specular))
-        for lighting in lightings:
-            gl.glMaterialfv(gl.GL_FRONT_AND_BACK, lighting[0], tuple(lighting[1]))
+    # clear screen & depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    # reset view to initialized view
+    glLoadIdentity()
+    glTranslatef(-1.5, 0.0, -6.0)
 
-        # set norms for triangles
-        # these are the norms for the surface, which is gross, but easy
-        for tri in ALIEN_1.triangles:
-            tri_norm = normal_vector(*tri)
-            for x, y, z in ((v.x, v.y, v.z) for v in tri):
-                gl.glNormal3f(*tri_norm)
-                gl.glVertex3f(x, y, z)
-            # # gl.glNormal3f(tri_norm.x, tri_norm.y, tri_norm.z)
-            # gl.glVertex3f(B.x, B.y, B.z)
-            # # gl.glNormal3f(tri_norm.x, tri_norm.y, tri_norm.z)
-            # gl.glVertex3f(C.x, C.y, C.z)
+    # rotate pyramid-building space by rtri on y axis
+    glRotatef(rtri, 0.0, 1.0, 0.0)
 
-    gl.glFlush()
+
+    glBegin(GL_TRIANGLES)
+
+    glColor3f(1.0, 0.0, 0.0)  # Red
+    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Front)
+    glColor3f(0.0, 1.0, 0.0)  # Green
+    glVertex3f(-1.0, -1.0, 1.0)  # Left Of Triangle (Front)
+    glColor3f(0.0, 0.0, 1.0)  # Blue
+    glVertex3f(1.0, -1.0, 1.0)
+
+    glColor3f(1.0, 0.0, 0.0)  # Red
+    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Right)
+    glColor3f(0.0, 0.0, 1.0)  # Blue
+    glVertex3f(1.0, -1.0, 1.0)  # Left Of Triangle (Right)
+    glColor3f(0.0, 1.0, 0.0)  # Green
+    glVertex3f(1.0, -1.0, -1.0)  # Right
+
+    glColor3f(1.0, 0.0, 0.0)  # Red
+    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Back)
+    glColor3f(0.0, 1.0, 0.0)  # Green
+    glVertex3f(1.0, -1.0, -1.0)  # Left Of Triangle (Back)
+    glColor3f(0.0, 0.0, 1.0)  # Blue
+    glVertex3f(-1.0, -1.0, -1.0)  # Right Of
+
+
+    glColor3f(1.0, 0.0, 0.0)  # Red
+    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Left)
+    glColor3f(0.0, 0.0, 1.0)  # Blue
+    glVertex3f(-1.0, -1.0, -1.0)  # Left Of Triangle (Left)
+    glColor3f(0.0, 1.0, 0.0)  # Green
+    glVertex3f(-1.0, -1.0, 1.0)  # Right Of Triangle (Left)
+    glEnd()
+
+    # What values to use?  Well, if you have a FAST machine
+    # and a FAST 3D Card, then large values make an unpleasant display
+    # with flickering and tearing.  I found that smaller values work better,
+    # but this was based on my experience.
+    rtri = rtri + 0.2  # Increase The Rotation Variable For The Triangle
+    rquad = rquad - 0.15  # Decrease The Rotation Variable For The Quad
+
+    # since this is double buffered, swap buffers to display what we drew
+    glutSwapBuffers()
 
 def get_display(gl, glut):
-    def display_func():
-        set_up(gl, glut)
-
-        with gl_push_and_pop(gl):
-            set_vertices(gl)
-
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-
-        # glut.glutPostRedisplay()
-        glut.glutSwapBuffers()
-    return display_func
-
+    return draw_scene
--- invaders/main.py
+++ invaders/main.py
@@ -25,16 +25,11 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL import GLU as glu
 
+import display
 import keyboard
 
 # Number of the glut window.
 window = 0
-
-# Rotation angle for the triangle.
-rtri = 0.0
-
-# Rotation angle for the quadrilateral.
-rquad = 0.0
 
 def gl_init(width, height):
     '''initialize OpenGL environment'''
@@ -68,57 +63,6 @@ def resize_func(width, height):
     glu.gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)
     glMatrixMode(GL_MODELVIEW)
 
-# The main drawing function.
-def DrawGLScene():
-    global rtri, rquad
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Clear The Screen And The Depth Buffer
-    glLoadIdentity()  # Reset The View
-    glTranslatef(-1.5, 0.0, -6.0)  # Move Left And Into The Screen
-
-    glRotatef(rtri, 0.0, 1.0, 0.0)  # Rotate The Pyramid On It's Y Axis
-
-    glBegin(GL_TRIANGLES)  # Start Drawing The Pyramid
-
-    glColor3f(1.0, 0.0, 0.0)  # Red
-    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Front)
-    glColor3f(0.0, 1.0, 0.0)  # Green
-    glVertex3f(-1.0, -1.0, 1.0)  # Left Of Triangle (Front)
-    glColor3f(0.0, 0.0, 1.0)  # Blue
-    glVertex3f(1.0, -1.0, 1.0)
-
-    glColor3f(1.0, 0.0, 0.0)  # Red
-    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Right)
-    glColor3f(0.0, 0.0, 1.0)  # Blue
-    glVertex3f(1.0, -1.0, 1.0)  # Left Of Triangle (Right)
-    glColor3f(0.0, 1.0, 0.0)  # Green
-    glVertex3f(1.0, -1.0, -1.0)  # Right
-
-    glColor3f(1.0, 0.0, 0.0)  # Red
-    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Back)
-    glColor3f(0.0, 1.0, 0.0)  # Green
-    glVertex3f(1.0, -1.0, -1.0)  # Left Of Triangle (Back)
-    glColor3f(0.0, 0.0, 1.0)  # Blue
-    glVertex3f(-1.0, -1.0, -1.0)  # Right Of
-
-
-    glColor3f(1.0, 0.0, 0.0)  # Red
-    glVertex3f(0.0, 1.0, 0.0)  # Top Of Triangle (Left)
-    glColor3f(0.0, 0.0, 1.0)  # Blue
-    glVertex3f(-1.0, -1.0, -1.0)  # Left Of Triangle (Left)
-    glColor3f(0.0, 1.0, 0.0)  # Green
-    glVertex3f(-1.0, -1.0, 1.0)  # Right Of Triangle (Left)
-    glEnd()
-
-    # What values to use?  Well, if you have a FAST machine
-    # and a FAST 3D Card, then large values make an unpleasant display
-    # with flickering and tearing.  I found that smaller values work better,
-    # but this was based on my experience.
-    rtri = rtri + 0.2  # Increase The Rotation Variable For The Triangle
-    rquad = rquad - 0.15  # Decrease The Rotation Variable For The Quad
-
-    # since this is double buffered, swap buffers to display what we drew
-    glutSwapBuffers()
 
 if __name__ == '__main__':
     glutInit(sys.argv)
@@ -132,12 +76,12 @@ if __name__ == '__main__':
     # assign to global window variable
     window = glutCreateWindow('Invaders')
 
-    glutDisplayFunc(DrawGLScene)
+    glutDisplayFunc(display.draw_scene)
     if config.fullscreen_mode:
         glutFullScreen()
 
     # redraw the scene between other calculations
-    glutIdleFunc(DrawGLScene)
+    glutIdleFunc(display.get_display(None, None))
     glutReshapeFunc(resize_func)
     glutKeyboardFunc(keyboard.normal_keys)
 
