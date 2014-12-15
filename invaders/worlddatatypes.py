11ae7db35d304110a50ab1eeb80feaea2bf2a1a3
Jim Witschey <jim.witschey@gmail.com>
2014-12-14 21:05:59 -0500

    simplifies position

--- invaders/worlddatatypes.py
+++ invaders/worlddatatypes.py
@@ -6,14 +6,7 @@ PositionBase = namedtuple('Position2', ('x', 'y'))
 
 class Position2(PositionBase):
     def __new__(cls, x, y):
-        return super(Position2, cls).__new__(cls,
-                                             x=cls._clip(x),
-                                             y=cls._clip(y))
-
-    def _clip(x):
-        return x
-        # return min(1, max(x, 0))
-
+        return super(Position2, cls).__new__(cls, x, y)
 
 class Owner(Enum):
     player = 1
