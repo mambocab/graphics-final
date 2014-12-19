6ced07fc99d6d868d5345bd9031c94256e4ebe3d
Jim Witschey <jim.witschey@gmail.com>
2014-12-19 15:36:24 -0500

    adds collision logic to utils.py

--- invaders/utils.py
+++ invaders/utils.py
@@ -1,3 +1,8 @@
-
 def tuple_replace(t, replace_at, value):
     return tuple(value if i == replace_at else v for i, v in enumerate(t))
+
+def collides(bullet, position, x_offsets, y_offsets):
+    x, y = position.x, position.y
+    collides_x = x + x_offsets[0] < bullet.position.x < x + x_offsets[1]
+    collides_y = y + y_offsets[0] < bullet.position.y < y + y_offsets[1]
+    return collides_x and collides_y
